c557e630fe842252ca72418eae53befbc45ab3e8
dominatorPZDC <klimovichklm@gmail.com>
2025-08-16 11:45:01 +0300

    initial commit

---
+++ src/test/java/org/example/twitterclone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
